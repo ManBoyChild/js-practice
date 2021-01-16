afc852945e0c51924bda355d164e9d1d909a12a3
Elliott Silverman <elliotts@ualberta.ca>
2021-01-16 01:12:50 -0700

    Completed this exerciese

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const strippedString = string.toLowerCase().replace(regex, '');
+    let arrayOfString =  [...strippedString];
+
+    let forwardIndex = 0;
+    let backwardIndex = arrayOfString.length - 1;
+
+    while(forwardIndex < backwardIndex) { 
+        if(arrayOfString[forwardIndex] === " ") {
+            forwardIndex += 1;
+        }
+        if(arrayOfString[backwardIndex] === " ") {
+            backwardIndex -= 1;
+        }
+        if (arrayOfString[forwardIndex] === arrayOfString[backwardIndex]) {
+            forwardIndex += 1;
+            backwardIndex -= 1;
+        } else {
+            return false;
+        }
+        return true;
+    }
 
 }
 

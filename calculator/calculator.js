0267bb74846948248959070655fc2387118a06bc
Elliott Silverman <elliotts@ualberta.ca>
2021-01-15 20:23:12 -0700

    Solved all problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arrayIn) {
+	let sum = 0;
+	for (let i = 0; i < arrayIn.length; i++) {
+		sum += arrayIn[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (arrayIn) {
+	let multiply = arrayIn.reduce((a, b) => a*b, 1);
+	return multiply;
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);	
 }
 
-function factorial() {
-	
+function factorial(factorial) {
+	let factorialArray = [];
+	if (factorial === 0 || factorial === 1) {
+		return 1;
+	}
+	for (let i = 1; i <= factorial; i++) {
+		factorialArray.push(i);
+	}
+	return factorialArray.reduce((a,b) => a*b);
 }
 
 module.exports = {

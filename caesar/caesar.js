d3c9c9470123a535b460d64a3b4023d0c1e71c2e
Elliott Silverman <elliotts@ualberta.ca>
2021-01-17 16:50:39 -0700

    Trying to figure out how to deal with large shifts

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,32 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let finalArray = [];
+
+
+    for (let i = 0; i < string.length; i++) {
+        let code = string.charCodeAt(i);
+        if (code >= 32 && code <= 64) {
+            finalArray.push(String.fromCharCode(code));
+        } else if (code >= 91 && code <= 96) {
+            finalArray.push(String.fromCharCode(code));
+        } else if (code >= 123 && code <= 126) {
+            finalArray.push(String.fromCharCode(code));
+        } else {
+            let shifted = code + shift;
+            if(shifted > 90 && shifted < 97) {
+                let remainder = shifted - 90;
+                shifted = 64 + remainder;
+            } else if (shifted > 122) {
+                 let remainder = shifted - 122;
+                shifted = 96 + remainder;
+            } else if (shifted < 65) {
+                let remainder = 65 - shifted;
+                shifted = 91 - remainder;
+            }
+            finalArray.push(String.fromCharCode(shifted));
+        }
+    }
+
+    return finalArray.join("");
 
 }
 

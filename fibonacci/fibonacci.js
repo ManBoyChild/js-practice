fd9cc34b5b78ab94660addd9bdc8f9989a6713f1
Elliott Silverman <elliotts@ualberta.ca>
2021-01-18 14:36:26 -0700

    All Problems Solved

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceVal) {
+    const fibValue = parseInt(sequenceVal);
+    let fibArray = [0, 1];
 
+    if (fibValue < 0) {
+        return "OOPS"
+    } else if (fibValue === 0) {
+        return 0;
+    } else if (fibValue === 1) {
+        return 1;
+    } else {
+        for (i = 2; i <= fibValue; i++) {
+            let next = fibArray[i - 1] + fibArray[i - 2];
+            fibArray.push(next);
+            
+        }
+        return fibArray[fibValue];
+    }
 }
 
 module.exports = fibonacci
